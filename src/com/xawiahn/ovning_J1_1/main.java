44f7ee5c40126c65019216f2577392ccd6ba4f4b
Xawiahn <xawiahn@gmail.com>
2012-08-24 09:28:23 +0200

    övning J1.3

--- src/com/xawiahn/ovning_J1_1/main.java
+++ src/com/xawiahn/ovning_J1_1/main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
+		/* ÖVNING J1.2
 		double tal1, tal2, tal3, summa, medelvardet;
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Skriv in tal1: ");
@@ -14,6 +15,29 @@ public class main {
 		summa = tal1 + tal2 + tal3;
 		medelvardet = summa / 3;
 		System.out.println("(" + tal1 + " + " + tal2 + " + " + " + " + tal3 + ") / 3 = " + medelvardet);
+		*/
+		
+		
+		/* ÖVNING J1.3
+		int tal1, tal2;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Skriv in tal1: ");
+		tal1 = sc.nextInt();
+		System.out.print("Skriv in tal2: ");
+		tal2 = sc.nextInt();
+		if(tal1 > tal2)
+		{
+			System.out.println(tal1 + " är större än " + tal2);
+		}
+		else if(tal1 == tal2)
+		{
+			System.out.println(tal1 + " och " + tal2 + " är lika stora");
+		}
+		else
+		{
+			System.out.println(tal2 + " är större än " + tal1);
+		}
+		*/
 	}
 
 }
